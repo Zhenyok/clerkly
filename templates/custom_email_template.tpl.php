acbefdaff9d94681f9ab631fbac4650b9ab45e0d
Eugene <e.radionov@itransition.com>
2017-02-10 18:45:33 +0300

    Log ip info correction and request message list page implementation

--- templates/custom_email_template.tpl.php
+++ templates/custom_email_template.tpl.php
@@ -1,10 +1,31 @@
-<div>
-    <p><?=t('Selected text: ');?><strong><?=$typo_text;?></strong></p>
-    <p><?=t('Field Machine Name: ');?><strong><?=$field_name;?></strong></p>
-    <p><?=t('Field Title: ');?><strong><?=$field_name_ui;?></strong></p>
-    <p><?=t('Node type: ');?><strong><?=$node_type;?></strong></p>
-    <p><?=t('Node link: ');?><strong><?=url("node/$nid");?></strong></p>
-    <p><?=t('Requested time: ');?><strong><?=$request_time;?></strong></p>
-    <p><?=t('User comment: ');?><strong><?=$comment_text;?></strong></p>
+<!DOCTYPE html>
+<html>
+    <head>
 
-</div>
+    </head>
+    <body>
+        <div>
+            <p><?=t('Selected text: ');?><strong><?=$typo_text;?></strong></p>
+            <p><?=t('Field Machine Name: ');?><strong><?=$field_name;?></strong></p>
+            <p><?=t('Field Title: ');?><strong><?=$field_name_ui;?></strong></p>
+            <p><?=t('Node type: ');?><strong><?=$node_type;?></strong></p>
+            <p><?=t('Node link: ');?><strong><?=url("node/$nid");?></strong></p>
+            <p><?=t('Requested time: ');?><strong><?=$date_created;?></strong></p>
+            <p><?=t('User comment: ');?><strong><?=$comment_text;?></strong></p>
+            <p><?=t('User IP: ')?>
+                <?php
+                    if(isset($ip)):
+                        echo $ip;
+                    endif;
+                ?>
+            </p>
+            <p><?=t('Username: ')?>
+                <?php
+                    if (isset($uid)):
+                        echo $uid;
+                    endif;
+                ?>
+            </p>
+        </div>
+    </body>
+</html>
